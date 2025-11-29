ece66e4d22c0cf295166a060841d6bedb0871631
ColeWhaley1 <colewhaley1@gmail.com>
2025-11-28 20:10:39 -0500

    Initial Commit

---
+++ NebRatings/NebRatingsApp.swift
@@ -0,0 +1,32 @@
+//
+//  NebRatingsApp.swift
+//  NebRatings
+//
+//  Created by Cole Whaley on 11/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NebRatingsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
